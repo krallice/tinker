1322e8e65b90b7f009da7ed8fc1ca434b422789a
root <root@client1.smithfield>
2016-07-16 14:18:35 +1000

    Adds base tinker.* code files

---
+++ tinker.c
@@ -0,0 +1,17 @@
+/* 
+ * File: tinker.c
+ * Author: github/krallice
+ * Date: July 2016
+ * Description: TODO
+ * 
+ * See the file "LICENSE" for the full license governing this code.
+*/
+
+#include "tinker.h"
+
+void main (int argc, char **argv[]) {
+
+	printf("Tinker Test\n");
+
+
+}
---
+++ tinker.h
@@ -0,0 +1,12 @@
+/* 
+ * File: tinker.h
+ * Author: github/krallice
+ * Date: July 2016
+ * Description: TODO
+ * 
+ * See the file "LICENSE" for the full license governing this code.
+*/
+
+#include <stdio.h> // Standard Printing/IO Functions
+#include <string.h> // String Manipulation Functions
+#include <stdlib.h> // Standard Functions including exit();
