f4c5ea73dda6dbbd2dc9e6da529ac6693dc36f2b
root <root@client1.smithfield>
2016-07-16 22:08:26 +1000

    Socket now binds to broadcast

--- tinker.c
+++ tinker.c
@@ -25,18 +25,6 @@ int main (int argc, char **argv[]) {
 	}
 
 	// Define Sockaddr structures for me (server) and other (clients):
-    struct sockaddr_in si_me, si_other;
-     
-    int s, i, slen = sizeof(si_other) , recv_len;
-    char buf[TINKER_BUFLEN];
-     
-    // Create a UDP/Datagram Socket: (s)
-    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-    {
-        die("socket");
-    }
-     
-    // Zero out our si_me structure (for safety), and then populate:
     // sockaddr_in prototype:
     // struct sockaddr_in {
     	// short            sin_family;   // e.g. AF_INET
@@ -47,27 +35,41 @@ int main (int argc, char **argv[]) {
 	// From http://www.gta.ufrj.br/ensino/eel878/sockets/sockaddr_inman.html: 
 	// With IPv4 (what basically everyone in 2005 still uses), the struct s_addr is a 4-byte number that represents one digit in an IP address per byte. 
 	// (You won't ever see an IP address with a number in it greater than 255.)
+    struct sockaddr_in si_me, si_other;
+     
+    int s, i, slen = sizeof(si_other) , recv_len;
+	int broadcast = 1;
+    char buf[TINKER_BUFLEN];
+     
+    // Create a UDP/Datagram Socket: (s)
+    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
+        die("socket");
+    }
 
+	// Allow Broadcast on Socket:
+	if ((setsockopt(s, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof broadcast)) == -1) {
+		die("setsockopt - SO_SOCKET ");
+	}
+     
+    // Zero out our si_me structure (for safety), and then populate:
     memset((char *) &si_me, 0, sizeof(si_me));
     si_me.sin_family = AF_INET;
     si_me.sin_port = htons(TINKER_PORT_SERVER);
     si_me.sin_addr.s_addr = htonl(INADDR_ANY);
      
     // Bind our UDP Datagram Socket to our sockaddr_in presenting the server:
-    if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
-    {
+    if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1) {
         die("bind");
     }
      
     // Listen for Packets:
-    while(1)
-    {
+    while(1) {
+
         printf("Waiting for data...\n");
         fflush(stdout);
          
         //try to receive some data, this is a blocking call
-        if ((recv_len = recvfrom(s, buf, TINKER_BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
-        {
+        if ((recv_len = recvfrom(s, buf, TINKER_BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1) {
             die("recvfrom()");
         }
          
@@ -77,7 +79,6 @@ int main (int argc, char **argv[]) {
 		
 		// Reset:
 		memset(buf, '\0', TINKER_BUFLEN);
-         
     }
  
     close(s);
