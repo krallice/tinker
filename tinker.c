1f9621129d3c4953c8aab84ef79779e5dadc3292
root <root@client1.smithfield>
2016-07-23 19:36:11 +1000

    Linked List Implemented for TransactionID Table

--- tinker.c
+++ tinker.c
@@ -9,6 +9,13 @@
 
 #include "tinker.h"
 
+// Linked list for our transaction table:
+typedef struct trans_tb {
+	unsigned char trans[4];
+	unsigned int count;
+	struct trans_tb * next;
+} trans_tb_t;
+
 void die(char *s) {
     perror(s);
     exit(1);
@@ -17,11 +24,83 @@ void die(char *s) {
 void udpListen() {
 }
 
+void print_transaction_table(trans_tb_t *head) { 
+
+	trans_tb_t * current = head;
+	
+	printf("Printing Transaction Table:\n");
+
+	while ( current != NULL ) {
+		printf("Node: ");
+		printf("Transaction ID: %02x%02x%02x%02x ", 	current->trans[0], 
+								current->trans[1], 
+								current->trans[2], 
+								current->trans[3]);
+		
+		printf("Current Count: %d ", current->count);
+		printf("Current Address: %p ", current);
+		printf("Next Address: %p\n", current->next);
+		current = current->next;
+	}
+}
+
+void add_transaction_table(trans_tb_t *head, unsigned char* t) {
+
+	trans_tb_t * current = head;
+	trans_tb_t * last = head;
+	int existing = 0;
+
+	// Traverse our existing linked list:
+	while ( current != NULL ) {
+		printf("loop\n");
+		// If existing:
+		if ( memcmp(current->trans, t, 4) == 0 ) {
+			printf("Match for %p, count was %d, is now %d\n", current, current->count, current->count+1);
+			current->count = current->count + 1;
+			existing = 1;
+		}
+		last = current;
+		current = current->next;
+	}
+
+	if ( ! existing ) {
+		if ( current  == NULL ) {
+			printf("Brand New Transaction");
+			last->next = malloc(sizeof(trans_tb_t));
+			memcpy(last->next->trans,t,4);
+			last->next->count = 1;
+			last->next->next = NULL;
+		}
+	}
+}
+
+void append_transaction_table(trans_tb_t *head, unsigned char* t) {
+
+	trans_tb_t * current = head;
+
+	// Get to the last element:
+	while ( current->next != NULL ) {
+		current = current->next;
+	}
+	
+	// Create our new element on the end:
+	current->next = malloc(sizeof(trans_tb_t));
+	memcpy(current->next->trans,t,4);
+	current->next->next = NULL;
+
+}
+
 int main (int argc, char **argv[]) {
 
 	if ( TINKER_DEBUG ) {
 		printf("Starting tinker dhcp ip server now...\n");
 	}
+	
+	// Init the first transaction table entry for our linked list:
+	trans_tb_t * trans_tb_head;
+	trans_tb_head = malloc(sizeof(trans_tb_t));
+	memset(trans_tb_head->trans, 0, sizeof(trans_tb_head));
+	trans_tb_head->next = NULL;
 
 	// Define Sockaddr structures for me (server) and other (clients):
 	// sockaddr_in prototype:
@@ -74,48 +153,58 @@ int main (int argc, char **argv[]) {
 		 
 		//print details of the client/peer and the data received
 		printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-		if ( buf[0] == DHCP_MSG_REQUEST ) {
-			printf("Recieved DHCP Message type of REQUEST\n");
-		}
+
+		// Only support ETHERNET Hardware for v1:
 		if ( buf[1] == DHCP_HW_ETH ) {
 			printf("Recieved DHCP Hardware Type of ETHERNET\n");
+
+			// If DHCP Request:
+			if ( buf[0] == DHCP_MSG_REQUEST ) {
+				printf("Recieved DHCP Message type of REQUEST\n");
+
+				// Client's MAC Address:
+				unsigned char clientmac[6];
+				memcpy(clientmac,&buf[28],6);
+				printf("Client MAC Address: %02x:%02x:%02x:%02x:%02x:%02x\n", clientmac[0],
+												clientmac[1],
+												clientmac[2],
+												clientmac[3],
+												clientmac[4],
+												clientmac[5],
+												clientmac[6]);
+				// Our transaction ID:
+				unsigned char trans[4];
+				memcpy(trans,&buf[4],4);
+				printf("Transaction ID: %02x%02x%02x%02x\n", trans[0], 
+										   trans[1], 
+										   trans[2], 
+										   trans[3]);
+
+				// Our elapsed seconds for transaction:
+				unsigned char sec[2];
+				memcpy(sec,&buf[8],2);
+				printf("Seconds Elapsed: %02d%02d\n", sec[0], sec[1]);
+
+				// Start playing with our transaction table:
+				add_transaction_table(trans_tb_head, trans);
+				//append_transaction_table(trans_tb_head, trans);
+				print_transaction_table(trans_tb_head);
+
+				// Entire DGRAM dump:
+				printf("Data:\n");
+				int i;
+				for ( i = 0; i <= TINKER_BUFLEN; i++ ) {
+					printf("%02x " , (unsigned)(unsigned char)buf[i]);
+				}
+				printf("End Data:\n");
+			
+			}
 		}
-
-		// Client's MAC Address:
-		unsigned char clientmac[6];
-		memcpy(clientmac,&buf[28],6);
-		printf("Client MAC Address: %02x:%02x:%02x:%02x:%02x:%02x\n", clientmac[0],
-										clientmac[1],
-										clientmac[2],
-										clientmac[3],
-										clientmac[4],
-										clientmac[5],
-										clientmac[6]);
-		// Our transaction ID:
-		unsigned char trans[4];
-		memcpy(trans,&buf[4],4);
-		printf("Transaction ID: %02x%02x%02x%02x\n", trans[0], 
-								   trans[1], 
-								   trans[2], 
-								   trans[3]);
-
-		// Our elapsed seconds for transaction:
-		unsigned char sec[2];
-		memcpy(sec,&buf[8],2);
-		printf("Seconds Elapsed: %02d%02d\n", sec[0], sec[1]);
-
-
-		// Entire DGRAM dump:
-		printf("Data:\n");
-		int i;
-		for ( i = 0; i <= TINKER_BUFLEN; i++ ) {
-			printf("%02x " , (unsigned)(unsigned char)buf[i]);
-		}
-		printf("End Data:\n");
-		
-		// Reset:
-		memset(buf, '\0', TINKER_BUFLEN);
+	
+	// Reset:
+	memset(buf, '\0', TINKER_BUFLEN);
 	}
+
 	close(s);
 	return 0;
 }
