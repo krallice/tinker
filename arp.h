153dce9ecdaeddd7b1b756cb28879b38ff164c0e
root <root@noclab.com.au>
2017-05-13 17:30:00 +1000

    arp header and c file

--- arp.h
+++ arp.h
@@ -23,4 +23,25 @@
 #include <linux/if_arp.h>
 #include <arpa/inet.h>
 
+#define PROTO_ARP 0x0806
+#define ETH2_HEADER_LEN 14
+#define HW_TYPE 1
+#define MAC_LENGTH 6
+#define IPV4_LENGTH 4
+#define ARP_REQUEST 0x01
+#define ARP_REPLY 0x02
+#define BUF_SIZE 60
+
+typedef struct arp_header {
+	unsigned short hardware_type;
+	unsigned short protocol_type;
+	unsigned char hardware_len;
+	unsigned char protocol_len;
+	unsigned short opcode;
+	unsigned char sender_mac[MAC_LENGTH];
+	unsigned char sender_ip[IPV4_LENGTH];
+	unsigned char target_mac[MAC_LENGTH];
+	unsigned char target_ip[IPV4_LENGTH];
+} arp_header_t;
+
 #endif // ARP_H
